03740f5989b00591d374c4f481b6604e9a6b3618
Anna Perlack <annaperlack@gmail.com>
2022-12-04 19:26:36 -0500

    deleted unnecessary HTML and added js with cocktail API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
+var btn = document.querySelector("#date-button");
 
+function getRecipeApi() {
+var requestUrl ='https://www.thecocktaildb.com/api/json/v1/1/random.php'
+fetch(requestUrl)
+    .then(function (response) {
+        console.log(response.json())
+    }).then(function (data) {
+       var randomCocktail = data.drinks[0].strDrink
+       console.log(randomCocktail)
+    }
+    )
+}
+
+btn.addEventListener("click", function (event) {
+    event.preventDefault()
+    getRecipeApi()
+    console.log(btn)
+})
