f52c7145927515d7a88b2d590d1539975dacad78
Anna Perlack <annaperlack@gmail.com>
2022-12-06 21:33:57 -0500

    set up working cocktail APIs to implement fetching cocktail by ingredient and getting its detail

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,43 @@
+var btn = document.getElementById("date-button");
+var alcoholListEl = document.getElementById("alcohol-list");
 
-function getRecipeApi() {
-var requestUrl ='https://www.thecocktaildb.com/api/json/v1/1/random.php'
-fetch(requestUrl)
-    .then(function (response) {
-        console.log(response.json())
-    }).then(function (data) {
-       var randomCocktail = data.drinks[0].strDrink
-       console.log(randomCocktail)
-    }
-    )
-}
+
+function getRecipeApi(alcohol) {
+    // console.log(alcohol)
+    var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + alcohol
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data)
+            var randomCocktail = data.drinks[Math.floor(Math.random()*data.drinks.length)];
+            console.log("random cocktail", randomCocktail)
+            getCocktailDetails(data.drinks[0].idDrink);
+        })
+};
 
 btn.addEventListener("click", function (event) {
+    var selectedAlcohol = alcoholListEl.value;
     event.preventDefault()
-    getRecipeApi()
-})
+    getRecipeApi(selectedAlcohol)
+    // console.log(selectedAlcohol);
+});
+
+function getCocktailDetails(id) {
+    var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + id
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data.drinks[0])
+            displayCocktailResults(data.drinks[0])
+        })
+};
+
+function displayCocktailResults(cocktail){
+    var drinkName = cocktail.strDrink
+    // var ingredients and measurements = cocktail.strIngredient
+    var recipe = cocktail.strInstructions
+    var picture = cocktail.strImageSource
+    console.log(drinkName,recipe,picture)
+};
